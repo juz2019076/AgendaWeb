f79b79e6cf9de2a70475b60694a45a044d21de79
juz2019076 <juz-2019076@kinal.edu.gt>
2024-02-06 00:51:23 -0600

    Modificando

--- assets/script/newContacto.js
+++ assets/script/newContacto.js
@@ -1,31 +1,6 @@
-const crearContacto = (parentNode, contacto, db) => {
-    let divContacto = document.createElement('div')
-    let nombreContacto = document.createElement('h3')
-    let numeroContacto = document.createElement('p')
-    let direccionContacto = document.createElement('p')
-    let iconoBorrar = document.createElement('span')
-
-    nombreContacto.innerHTML = contacto.nombre
-    numeroContacto.innerHTML = contacto.numero
-    direccionContacto.innerHTML = contacto.direccion
-    iconoBorrar.innerHTML = ' delete_forever'
-
-    divContacto.classList.add('Tarea')
-    iconoBorrar.classList.add('material-icons', 'icono')
-
-    iconoBorrar.onclick = () => {
-        db.removeItem(contacto.id)
-        window.location.href = '/'
-
-    }
-
-
-    divContacto.appendChild(nombreContacto)
-    divContacto.appendChild(numeroContacto)
-    divContacto.appendChild(direccionContacto)
-    divContacto.appendChild(iconoBorrar)
-
-    parentNode.appendChild(divContacto)
-
-
-}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('nuevoContactoForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Contacto agregado exitosamente');
+    });
+});
