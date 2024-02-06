7766e8c68095e7c2d05dd3dbfbd6c148e7f4585f
juz2019076 <juz-2019076@kinal.edu.gt>
2024-02-05 23:41:19 -0600

    Modificando

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,6 +1,4 @@
 function NuevaPagina() {
-    // Puedes redirigir a la página siguiente aquí
     alert("Redirigiendo a la página siguiente...") ;
-    // Por ejemplo:
      window.location.href = "../pages/contactos.html";
   }
